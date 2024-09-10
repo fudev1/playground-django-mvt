3b68a8612cbd3e09eb99e9164f488e8af995c656
Matteo D.V. <matteo.divita@outlook.be>
2024-09-10 23:02:16 +0200

    feature: paginator in tickets

--- videotheque/tickets/views.py
+++ videotheque/tickets/views.py
@@ -3,6 +3,7 @@ from .models import *
 from .forms import *
 from django.contrib.auth.decorators import login_required # Permet de vérifier si l'utilisateur est connecté 
 from django.http import HttpResponseForbidden
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -23,8 +24,21 @@ def creer_ticket(request):
 
 
 
+
+
+"""
+Si on avait plusieurs tickets on pourrait utiliser la pagination et éviter de charger trop d'éléments dans la page
+=> Utiliser Django Paginator (import)
+"""
+# def liste_tickets(request):
+#     tickets = Ticket.objects.all()
+#     return render(request, 'liste_tickets.html', {'tickets': tickets})
+
 def liste_tickets(request):
-    tickets = Ticket.objects.all()
+    tickets_liste = Ticket.objects.all()
+    paginator = Paginator(tickets_liste, 10)
+    page_number = request.GET.get('page')
+    tickets = paginator.get_page(page_number)
     return render(request, 'liste_tickets.html', {'tickets': tickets})
 
 
